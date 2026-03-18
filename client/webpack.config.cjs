581e2e5b1551e03b179f0bcbe5314e8993bf3ca7
Aly Albahri <muntasiralbahri@gmail.com>
2026-03-18 19:37:49 +0400

    Client Folder Setup & Modal Component

---
+++ client/webpack.config.cjs
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, '../public'),
+    filename: 'bundle.js', // Note: Your HTML must now look for "bundle.js" instead of "main.js"
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, '../public'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'], // 2. Replace 'style-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack App',
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'main.css', // 3. This is the name of your output CSS file
+    }),
+  ],
+};
